12bb89f78fc387724d5ec311de2946ae7d782d1f
ziclari <ziclari@users.noreply.github.com>
2025-05-06 03:31:39 +0000

    deploy: 755a3d01b1a76f36e6afbf2f7f29771bd7d0212a

---
+++ Main.scala
@@ -0,0 +1,8 @@
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("Main")
+object Main {
+    def main(args: Array[String]): Unit = {
+        println("¡Bienvenido a La Vecindad de los Secretos!")
+    }
+}
---
+++ build.sbt
@@ -0,0 +1,7 @@
+enablePlugins(ScalaJSPlugin)
+
+scalaVersion := "3.3.3"
+name := "VecindadDeLosSecretos"
+version := "0.1.0"
+
+scalaJSUseMainModuleInitializer := true
